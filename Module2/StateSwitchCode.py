b1bb0c4ea7ce31442d24789f718c7ee7a63224ef
devitos-yale <sophia.devito@yale.edu>
2021-09-27 19:43:36 -0400

    add initial state

--- Module2/StateSwitchCode.py
+++ Module2/StateSwitchCode.py
@@ -3,11 +3,11 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 #GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(18, GPIO.IN) #button
-GPIO.setup(22, GPIO.IN) #switch
-GPIO.setup(15, GPIO.IN) #joystick x
-GPIO.setup(13, GPIO.IN) #joystick y
-GPIO.setup(11, GPIO.IN) #joystick switch
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #button
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #switch
+GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #joystick x
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #joystick y
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #joystick switch
 
 #try:
 while True: # Run forever
