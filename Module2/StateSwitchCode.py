e06446c4a4379a29ef2a74e2e30c4ee7b51cd12e
devitos-yale <sophia.devito@yale.edu>
2021-09-27 19:45:07 -0400

    updates for testing

--- Module2/StateSwitchCode.py
+++ Module2/StateSwitchCode.py
@@ -4,9 +4,9 @@ GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 #GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #button
-GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #switch
+GPIO.setup(22, GPIO.IN) #switch
 GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #joystick x
-GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #joystick y
+GPIO.setup(13, GPIO.IN) #joystick y
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #joystick switch
 
 #try:
@@ -17,11 +17,11 @@ while True: # Run forever
 	if GPIO.input(22) == GPIO.HIGH:
 		print("Switch is high")
 
-	if GPIO.input(15) != GPIO.LOW:
+	if GPIO.input(15) == GPIO.HIGH:
 		print("Joystick x")
-	if GPIO.input(13) != GPIO.LOW:
+	if GPIO.input(13) == GPIO.HIGH:
 		print("Joystick y")
-	if GPIO.input(11) != GPIO.HIGH:
+	if GPIO.input(11) == GPIO.HIGH:
 		print("Joystick switch")
 
 #finally:
