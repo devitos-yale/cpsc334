9ef3b3c4a79ef47e4fb0612458d527605480d2ca
devitos-yale <sophia.devito@yale.edu>
2021-09-27 19:35:50 -0400

    updated GPIO pin numbers

--- Module2/StateSwitchCode.py
+++ Module2/StateSwitchCode.py
@@ -3,25 +3,25 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 #GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(10, GPIO.IN)
-GPIO.setup(2, GPIO.IN)
-GPIO.setup(3, GPIO.IN)
-GPIO.setup(3, GPIO.IN)
-GPIO.setup(3, GPIO.IN)
+GPIO.setup(18, GPIO.IN) #button
+GPIO.setup(22, GPIO.IN) #switch
+GPIO.setup(15, GPIO.IN) #joystick x
+GPIO.setup(13, GPIO.IN) #joystick y
+GPIO.setup(11, GPIO.IN) #joystick switch
 
 try:
 	while True: # Run forever
-		button_input = GPIO.input(10)
+		button_input = GPIO.input(18)
     	if button_input == GPIO.HIGH:
        		print("Button was pushed")
 
-    	switch_input = GPIO.input(2)
+    	switch_input = GPIO.input(22)
     	if switch_input == GPIO.HIGH:
         	print("Switch is high")
 
-        joystick_input_x = GPIO.input(3)
-        joystick_input_y = GPIO.input(4)
-        joystick_input_sw = GPIO.input(5)
+        joystick_input_x = GPIO.input(15)
+        joystick_input_y = GPIO.input(13)
+        joystick_input_sw = GPIO.input(11)
 
         if joystick_input_x != GPIO.LOW:
         	print("Joystick x")
