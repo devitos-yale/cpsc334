88d24711f9b41e84618293985608edad82d53e26
devitos-yale <sophia.devito@yale.edu>
2021-10-01 15:16:48 -0400

    Create ESP32DecodeTrial.py

---
+++ Module2/ESP32DecodeTrial.py
@@ -0,0 +1,40 @@
+import serial
+
+ser = serial.Serial()
+ser.port = '/dev/cu.SLAB_USBtoUART'
+
+#ser.baudrate = 115200
+#ser.setDTR(False)
+#ser.setRTS(False)
+
+ser.open()
+device = 1;
+
+while True:
+	line = ser.readline()
+	decoded_line = (line[0:len(line)-2].decode("utf-8"))
+	#print(str(device) + ", " + str(decoded_line))
+
+	#if (str(decoded_line) == 'NEW'):
+		#device = 1
+
+	#if (device == 1):
+
+	#if (device == 2):
+
+	if (device == 3):
+		if (int(decoded_line) == 0):
+			print('JOYSTICK PRESSED')
+
+	if (device == 4):
+		if (int(decoded_line) == 0):
+			print('BUTTON PRESSED')
+
+	if (device == 5):
+		if (int(decoded_line) == 0):
+			print('SWITCH PRESSED')
+
+	if (device == 5):
+		device = 1
+	else:
+		device = device+1
